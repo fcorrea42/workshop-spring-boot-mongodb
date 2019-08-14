df9d11653a57a85ffaa8ec739349da3a5dbadc4f
Felipe de Freitas Correa <fcorreafelipe@gmail.com>
2019-08-14 16:43:16 -0300

    Obtendo um usuario por id

---
+++ src/main/java/com/fcorrea42/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,10 @@
+package com.fcorrea42.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+}
