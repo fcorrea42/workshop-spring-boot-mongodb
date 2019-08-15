0b90b884b4f0f0aec3ca27fdd250f798c06ab4eb
Felipe de Freitas Correa <fcorreafelipe@gmail.com>
2019-08-15 15:57:32 -0300

    Consulta simples com @Query

--- src/main/java/com/fcorrea42/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/fcorrea42/workshopmongo/repository/PostRepository.java
@@ -3,11 +3,17 @@ package com.fcorrea42.workshopmongo.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fcorrea42.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	// Codificação do regex: "{ <field>: { $regex: /pattern/, $options: '<options>' } }"
+	// ?0 = primeiro parâmetro que entrar na função; i = case insensitive
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+	List<Post> searchTitle(String text);
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
